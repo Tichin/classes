f56e1f9013fad1fbf1b7711aa129336729266dfd
Didi <devdidi5@gmail.com>
2022-10-17 15:26:16 -0700

    Modify classes/index.js and relared components

---
+++ components/authButton.js
@@ -0,0 +1,9 @@
+export default function AuthButton() {
+  return (
+    <>
+      <button className="hover:bg-[#ffbf00] shadedow-lg border-solid border-color: inherit; border-[1px] pl-4 pr-4 bg-[#f1be32] text-black">
+        Sign in
+      </button>
+    </>
+  );
+}
